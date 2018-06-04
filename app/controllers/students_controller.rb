c01d8b2c054eb5fc5bebbc661af16f718462be02
stritch.dev <stritch.dev@gmail.com>
2018-06-04 12:43:17 -0500

    passing most tests

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,18 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  # before_action :set_student, only: :show
   
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+    # "Daenerys"
   end
 
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+    # def set_student
+      # @student = Student.find(params[:id])
+    # end
 end
